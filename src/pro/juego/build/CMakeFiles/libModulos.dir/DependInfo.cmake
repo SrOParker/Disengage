5efa017ed5b4299af06ca560d5ca78419796c7f6
Laureano <63461668+SrOParker@users.noreply.github.com>
2022-05-29 21:12:34 +0200

    Add files via upload

---
+++ src/pro/juego/build/CMakeFiles/libModulos.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Armas.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Armas.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Enemigos.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Enemigos.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Escena.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Escena.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Hud.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Hud.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Juego.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Juego.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Linterna.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Linterna.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Mapa.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Mapa.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Menu.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Menu.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Motor2D.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Motor2D.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Nivel.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Nivel.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Npc_Mejora.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Npc_Mejora.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Npc_desbloqueo_objetos.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Npc_desbloqueo_objetos.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/O_Trampa.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/O_Trampa.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/O_ataque.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/O_ataque.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/O_maldito.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/O_maldito.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/O_rango.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/O_rango.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/O_vAtaque.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/O_vAtaque.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/O_velocidad.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/O_velocidad.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/O_vida.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/O_vida.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Objeto.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Objeto.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Partida.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Partida.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/PersonajeBase.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/PersonajeBase.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/PersonajePrincipal.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/PersonajePrincipal.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/Sprite1.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/Sprite1.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/tinystr.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/tinystr.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/tinyxml.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/tinyxml.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/tinyxmlerror.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/tinyxmlerror.cpp.o"
+  "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/ej_modulos/tinyxmlparser.cpp" "/home/osboxes/Escritorio/FV/proyectoabp-grupo-b5/src/pro/juego/build/CMakeFiles/libModulos.dir/ej_modulos/tinyxmlparser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ej_modulos"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/pro/juego/build/CMakeFiles/libModulos.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libModulos.dir/ej_modulos/Motor2D.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Sprite1.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Partida.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Nivel.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Menu.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Escena.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Objeto.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/PersonajeBase.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/PersonajePrincipal.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/O_ataque.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/O_rango.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/O_vAtaque.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/O_velocidad.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/O_vida.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Armas.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Hud.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Enemigos.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Mapa.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/tinystr.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/tinyxml.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/tinyxmlerror.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/tinyxmlparser.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Linterna.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Npc_Mejora.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Npc_desbloqueo_objetos.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/O_Trampa.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/O_maldito.cpp.o"
+  "CMakeFiles/libModulos.dir/ej_modulos/Juego.cpp.o"
+  "liblibModulos.pdb"
+  "liblibModulos.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libModulos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
